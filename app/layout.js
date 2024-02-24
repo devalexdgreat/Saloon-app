977b530695f727794238a3fb10cb57a48cbd5823
devadg <opponamer@gmail.com>
2024-02-24 16:30:26 +0100

    done

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="overflow-x-hidden relative">
+    <html lang="en" className="overflow-x-hidden">
       <body className={font.className}>
         <Navbar />
         {/* <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-orange-500 opacity-20 blur-[100px]"></div></div> */}
